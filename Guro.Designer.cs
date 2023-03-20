f8946d370be5760fd1442025e2e3fdf61c348dc9
walidGrlm <126637404+walidGrlm@users.noreply.github.com>
2023-03-20 03:50:12 +0000

    Add files via upload

---
+++ Guro.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Wishlist = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Fill = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Count = new System.Windows.Forms.Button();
+            this.Seliction = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Set = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Wishlist
+            // 
+            this.Wishlist.FormattingEnabled = true;
+            this.Wishlist.Location = new System.Drawing.Point(12, 36);
+            this.Wishlist.Name = "Wishlist";
+            this.Wishlist.Size = new System.Drawing.Size(151, 147);
+            this.Wishlist.TabIndex = 0;
+            this.Wishlist.SelectedIndexChanged += new System.EventHandler(this.Wishlist_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Wishlist";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Fill
+            // 
+            this.Fill.Location = new System.Drawing.Point(234, 36);
+            this.Fill.Name = "Fill";
+            this.Fill.Size = new System.Drawing.Size(75, 23);
+            this.Fill.TabIndex = 2;
+            this.Fill.Text = "Fill";
+            this.Fill.UseVisualStyleBackColor = true;
+            this.Fill.Click += new System.EventHandler(this.Fill_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(234, 119);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Count
+            // 
+            this.Count.Location = new System.Drawing.Point(234, 160);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(75, 23);
+            this.Count.TabIndex = 4;
+            this.Count.Text = "Count";
+            this.Count.UseVisualStyleBackColor = true;
+            this.Count.Click += new System.EventHandler(this.Count_Click);
+            // 
+            // Seliction
+            // 
+            this.Seliction.AutoSize = true;
+            this.Seliction.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Seliction.Location = new System.Drawing.Point(12, 209);
+            this.Seliction.Name = "Seliction";
+            this.Seliction.Size = new System.Drawing.Size(60, 13);
+            this.Seliction.TabIndex = 5;
+            this.Seliction.Text = "Selection";
+            this.Seliction.Click += new System.EventHandler(this.Seliction_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Count";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 206);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 239);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Set
+            // 
+            this.Set.Location = new System.Drawing.Point(234, 75);
+            this.Set.Name = "Set";
+            this.Set.Size = new System.Drawing.Size(75, 23);
+            this.Set.TabIndex = 9;
+            this.Set.Text = "Sort";
+            this.Set.UseVisualStyleBackColor = true;
+            this.Set.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(234, 209);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 10;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 321);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Set);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Seliction);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Fill);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Wishlist);
+            this.Name = "Form1";
+            this.Text = "Wishlist";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Wishlist;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Fill;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Count;
+        private System.Windows.Forms.Label Seliction;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Set;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ Guro.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Wishlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = Wishlist.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Seliction_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Wishlist.Sorted = true; 
+        }
+
+        private void Fill_Click(object sender, EventArgs e)
+        {
+            Wishlist.Items.Add("Rojo");
+            Wishlist.Items.Add("House");
+            Wishlist.Items.Add("Full Money ");
+            Wishlist.Items.Add("iPhone Pro Max 14 ");
+            Wishlist.Items.Add("Car");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Wishlist.Items.Clear();
+        }
+
+        private void Count_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Convert.ToString(Wishlist.Items.Count);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+    }
+}
